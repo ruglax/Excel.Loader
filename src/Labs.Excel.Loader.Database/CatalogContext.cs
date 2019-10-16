659370cf933ac64d0e80388b8267fa618c6ff2bd
Raúl Silva <raul.sr.87@gmail.com>
2019-10-16 07:15:38 -0500

    Added dbcontext

---
+++ src/Labs.Excel.Loader.Database/CatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labs.Excel.Loader.Database
+{
+    public class CatalogContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Labs.Excel.Loader.Database/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labs.Excel.Loader.Database
+{
+    public class Repository<T> where T : class, new()
+    {
+        private readonly DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public void BulkInsert(T[] entities)
+        {
+            _context.BulkInsert(entities);
+        }
+    }
+}
